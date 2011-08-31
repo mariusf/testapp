dbba96a19d3a96736c155002bc2caa49d2938e95
Marius Flueckiger <marius.flueckiger@innocube.ch>
2011-08-31 11:37:51 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobile::Application.config.secret_token = '10ec9af68d818e0d08e3700160f12169290aeae76d65b769946bcc11769fa3de606acfada7b345f1a63691913c3e8ce98d24ae9a4f23ddcfa7fcbed117b2b17b'
